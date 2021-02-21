df3075cc6b6c1e582ade91119fe02bf00d32c0c1
ValeriiMatviienko <unanimous276@gmail.com>
2021-02-21 14:22:30 +0200

    add files

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import defImg from '../../img/default.png';
+import s from './FriendListItem.module.css';
+
+function FriendListItem({
+  avatar = defImg,
+  name = 'unknown',
+  isOnline = true,
+}) {
+  return (
+    <li className={s.item}>
+      <span className={isOnline ? `${s.isOnline}` : `${s.isOffline}`}></span>
+      <img className={s.avatar} src={avatar} alt={name} width="48" />
+      <p className={s.name}>{name}</p>
+    </li>
+  );
+}
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool,
+};
+
+export default FriendListItem;
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TransactionHistoryItem from './TransactionHistoryItem';
+import s from './TransactionHistory.module.css';
+
+function TransactionHistory({ items }) {
+  return (
+    <table className={s.history}>
+      <thead className={s.heading}>
+        <tr>
+          <th className={s.cell}>Type</th>
+          <th className={s.cell}>Amount</th>
+          <th className={s.cell}>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <TransactionHistoryItem
+            key={id}
+            type={type}
+            amount={amount}
+            currency={currency}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+TransactionHistory.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
+
+export default TransactionHistory;
